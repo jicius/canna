823cf3008633c38aac38b43c59d699c482e6a3a2
jicius <bq_ji@yahoo.com>
2017-07-19 09:15:40 +0800

    add app instance to wsgi

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -15,3 +15,8 @@
 # 
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from {{ cookiecutter.project_name }} import app
+
+# 开启调试模式
+app.config.update(DEBUG=True)
